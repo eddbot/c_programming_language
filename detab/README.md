cec92679b635731fdbbca4f9ae1ceea101248644
eddbot <edwardsansome@gmail.com>
2025-01-12 00:06:44 +0000

    init + detab exercise

---
+++ detab/README.md
@@ -0,0 +1,3 @@
+# detab
+
+Write a program that replaces tabs in the input with the correct number of spaces.
--- detab/main.c
+++ detab/main.c
@@ -5,28 +5,28 @@
 int main(){
 
   int c;
-  int lineLength = 0;
+  int line_length = 0;
 
   while((c = getchar()) != EOF) {
     if(c == '\t'){
       // calculate spaces required by segmenting the line based on it's length % TABSTOP
-      int spaces_required = TABSTOP - (lineLength % TABSTOP);
+      int spaces_required = TABSTOP - (line_length % TABSTOP);
 
       // append the chars to the line
       while(--spaces_required >= 0) putchar(' ');
 
       // reset the line length
-      lineLength = 0;
+      line_length = 0;
     } 
     else if(c == '\n'){
       // reset the line length as we're moving to a new line
-      lineLength = 0;
+      line_length = 0;
 
       // actually print the newline char
       putchar('\n');
     }
     else {
-      ++lineLength;
+      ++line_length;
       putchar(c);
     }
   }
